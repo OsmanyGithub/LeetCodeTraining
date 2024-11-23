8f402720827007be905c1fe3252fdde6171e03fd
Osmany <158761806+OsmanyGithub@users.noreply.github.com>
2024-11-23 02:47:19 -0800

    Time: 0 ms, Memory: 40.6 MB - LeetHub

--- perfect-squares/perfect-squares.java
+++ perfect-squares/perfect-squares.java
@@ -1,42 +1,37 @@
 class Solution {
     public int numSquares(int n) {
-        // Step 1: Initialize the queue and visited set
-        Queue<Integer> queue = new LinkedList<>();
-        Set<Integer> visited = new HashSet<>();
+        // Solution based on Lagrange's Four Square Theorem 
+        //`Every positive integer can be represented as the sum of four squares,
+        // or less.`
+        // Time complexity O(√n) && Space complexity O(1)
         
-        // Step 2: Add teh starting number `n` to the queue and visited set
-        queue.add(n);
-        visited.add(n);
+        // if n <= 0, answer is 0
+        if (n <= 0) return 0;
         
-        int level = 0; // BFS level (number of perfect squares)
+        // if n is a perfect square, answer is 1
+        if (isPerfectSquare(n)) return 1;
         
-        
-        // Perform BFS
-        while(!queue.isEmpty()){
-            level ++;
-            int size = queue.size();
-            
-            // Process all nodes in the current level            
-            for(int i = 0; i < size; i ++){
-                int curr = queue.poll();
-                
-                // Generate neighbors                
-                for (int j = 1; j * j <= curr; j ++){
-                    int next = curr - j * j;
-                    
-                    // If we reach 0, we found the least of perfect square sum for `n`
-                    if (next == 0) return level;
-                    
-                    // Add invisited nodes to the queue and set                    
-                    if (!visited.contains(next)){
-                        queue.offer(next);
-                        visited.add(next);
-                    }
-                }
+        // if n is the result of add two perfect square number, the answer is 2
+        for(int i = 1; i*i <= n; i ++){
+            if (isPerfectSquare(n - i*i)){
+                return 2;
             }
         }
         
-        return level;
+        // if n = 4^a(8b + 7), the answer is 4
+        while (n % 4 == 0) n /= 4;
         
+        if (n % 8 == 7) return  4;
+
+        
+        // If non of the above are valid, the answer is 3
+        return 3;
+        
+    }
+    
+    // Helper method for determine if a number is a perfect square
+    private boolean isPerfectSquare(int x){
+        int sqrt = (int) Math.sqrt(x);
+        return sqrt * sqrt == x;
     }
 }
