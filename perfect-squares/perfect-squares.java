3c2fb7c923126f7a35d2f3dc5efc138c037e54ae
Osmany <158761806+OsmanyGithub@users.noreply.github.com>
2024-11-23 02:57:04 -0800

    Time: 0 ms, Memory: 40.8 MB - LeetHub

--- perfect-squares/perfect-squares.java
+++ perfect-squares/perfect-squares.java
@@ -1,5 +1,8 @@
 class Solution {
     public int numSquares(int n) {
+        // Update move the for Loop after the while check,
+        // this avoids unnecessary computations
+        
         // Solution based on Lagrange's Four Square Theorem 
         //`Every positive integer can be represented as the sum of four squares,
         // or less.`
@@ -11,19 +14,18 @@ class Solution {
         // if n is a perfect square, answer is 1
         if (isPerfectSquare(n)) return 1;
         
-        // if n is the result of add two perfect square number, the answer is 2
+        // if n = 4^a(8b + 7), the answer is 4
+        while (n % 4 == 0) n /= 4;
+        
+        if (n % 8 == 7) return  4;
+        
+         // if n is the result of add two perfect square number, the answer is 2
         for(int i = 1; i*i <= n; i ++){
             if (isPerfectSquare(n - i*i)){
                 return 2;
             }
         }
-        
-        // if n = 4^a(8b + 7), the answer is 4
-        while (n % 4 == 0) n /= 4;
-        
-        if (n % 8 == 7) return  4;
 
-        
         // If non of the above are valid, the answer is 3
         return 3;
         
