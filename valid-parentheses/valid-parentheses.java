0f769f893a1aeea08feff9567169ffbb353d44d1
Osmany <158761806+OsmanyGithub@users.noreply.github.com>
2024-11-25 19:41:24 -0800

    Time: 2 ms, Memory: 41.8 MB - LeetHub

--- valid-parentheses/valid-parentheses.java
+++ valid-parentheses/valid-parentheses.java
@@ -1,23 +1,30 @@
 class Solution {
     public boolean isValid(String s) {
+        // Use stack for store the open brackets
         Stack<Character> stack = new Stack<>();
+        
+        // Travers the String s converted in a Char Array to easly compare characters;
         for (char c: s.toCharArray()){
             if (c == ')' || c == '}' || c == ']'){
+                // If stack is empty when we find a close bracket the String is invalid
                 if (stack.isEmpty()) {
                     return false;
                 }
                 
+                // Otherwise remove from the stack to check the match                
                 char match = stack.pop();
                 if(c == ')' && match != '(' || c == '}' && match != '{' || c == ']' && match != '['){
                     return false; 
                 }
                 
             }
+            // Store in the stack all the open brackets
             else if(c == '(' || c == '{' || c == '['){
                 stack.push(c);
             }
         }
         
+        // If at the end of the loop the stack has any character the String is invalid        
         return stack.isEmpty();
         
     }
