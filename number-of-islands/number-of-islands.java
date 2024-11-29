07b101a21a44a0db5ee6c7d682acda8fe427b890
Osmany <158761806+OsmanyGithub@users.noreply.github.com>
2024-11-29 01:12:06 -0800

    Time: 6 ms, Memory: 49.2 MB - LeetHub

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -1,60 +1,42 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) return 0;
-        int n = grid.length; // Number of rows
-        int m = grid[0].length; // Number of colums
+        if (grid == null || grid.length == 0){
+            return 0;
+        }
         
-        // Boolean array to track the visited land cells
-        boolean [][] visited = new boolean[n][m]; 
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
         
-        int islandCount = 0; // Island counter
+        boolean [][] visited = new boolean[m][n];
         
-        // Loop through the grid to find land cells
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j ++){
-                // If an unvisited land is found
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
                 if (grid[i][j] == '1' && !visited[i][j]){
-                    // Perform a bfs to mark all conected land cells
-                    bfs(grid, visited, i, j, n, m);
-                    
-                    islandCount ++; // Increment the island counter
+                    count++;
+                    dfs(grid, visited, i, j, m, n);
                 }
-            }
-        }
-        
-        return islandCount;
-    }
-    
-    // Helper Method Breath First Search
-    public void bfs(char[][] grid, boolean [][] visited, int i, int j, int n, int m){
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{i, j}); // Add the land cell to the queue
-        visited[i][j] = true; // Mark as visited
-        
-        // Array of directions (up, right, down, left)
-        int [] directions = {-1 , 0, 1, 0, -1};
-        
-        while (!queue.isEmpty()){
-            int [] currentCell = queue.poll();
-            int row = currentCell[0];
-            int col = currentCell[1];
-            
-            // Move in the 4 directions
-            for(int index = 0; index < 4; index++){
-                int nextRow = row + directions[index];
-                int nextCol = col + directions[index + 1];
                 
-                // Check if the new cell is in bound and if it's an unvisited island
-                if (nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < m 
-                   && grid[nextRow][nextCol] == '1' && !visited[nextRow][nextCol]){
-                    // Add land to the queue
-                    queue.add(new int[]{nextRow, nextCol});
-                    // Mark the cell as visited
-                    visited[nextRow][nextCol] = true;
-                }
             }
         }
+        return count;
+    }
+    // Helper function dfs
+    private void dfs(char[][] grid, boolean[][] visited, int i, int j, int m, int n){
+        // Directions
+        int [] directions = {-1, 0, 1, 0, -1};
         
-        
+        // Mark cell as visited
+        visited[i][j] = true;
+
+        for(int indx = 0; indx < 4; indx ++){
+            int nextRow = i + directions[indx];
+            int nextCol = j + directions[indx + 1];
+
+            if (nextRow >= 0 && nextRow < m && nextCol >= 0 && nextCol < n 
+                && grid[nextRow][nextCol] == '1' && !visited[nextRow][nextCol]){
+                dfs(grid, visited, nextRow, nextCol, m, n);
+            }
+        }
     }
 }
