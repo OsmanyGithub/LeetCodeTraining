7e066343a0ff33ff79e114b47fc01e4a975a11f6
Osmany <158761806+OsmanyGithub@users.noreply.github.com>
2024-12-28 05:36:43 -0800

    Time: 2 ms, Memory: 43.4 MB - LeetHub

--- intersection-of-two-arrays/intersection-of-two-arrays.java
+++ intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,43 +1,35 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        // Create a Set to store the values of the smaller array
-        Set<Integer> seen = new HashSet<>();
-        Set<Integer> match = new HashSet<>();
+        // Compare arrays to get the largest and shortest array
+        int [] shortestArray = (nums1.length < nums2.length) ? nums1 : nums2;
+        int [] largestArray = (nums1.length < nums2.length) ? nums2 : nums1;
         
-        // Put the values of the smaller array into the Set;
-        if (nums1.length < nums2.length){
-            for (int n: nums1){
-                seen.add(n);
-            }
-            // Travers the longest array
-            for (int n: nums2){
-        // If seen has the same number, store in the first available index of result & increase index value
-                if (seen.contains(n)){
-                    if (!match.contains(n)) {
-                        match.add(n);                       
-                    } 
-                }
-            } 
+        // Create the Set seen to store values of the shortest array        
+        Set<Integer> seen = new HashSet<>();
+        for (int n: shortestArray){
+            seen.add(n);
         }
-        else{
-            for (int n : nums2){
-                seen.add(n);
-            }
-            for (int n: nums1){
-                if(seen.contains(n)){
-                    if (!match.contains(n)){
-                        match.add(n);
-                    }
-                }
+        
+        // Create the Set intersection to store the numbers that appears in both arrays         
+        Set<Integer> intersection = new HashSet<>();
+        for (int n: largestArray){
+            if (seen.contains(n)){
+                intersection.add(n);
             }
         }
-        // Get the size of match Set
-        int size = match.size();
-        int []result = new int[size]; // Initialize result array using the previous size value
-        int i = 0; // First available position in result
-        for (int n: match){
-            result[i++] = n;
+        
+        // Get the size of intersection       
+        int size = intersection.size(); 
+        // Create the Array result for store all the numbers of intersection
+        int [] result = new int[size]; 
+        
+        // Add all numbers of Set intersection into the Array result
+        int i = 0;
+        for (int n: intersection){
+            result[i] = n;
+            i++;
         }
+        
         return result;
     }
 }
