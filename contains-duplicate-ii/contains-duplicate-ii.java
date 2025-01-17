d5587994e00c28f8e83bc0da484d3f8a7e5bfaae
Osmany <158761806+OsmanyGithub@users.noreply.github.com>
2025-01-16 19:19:41 -0800

    Time: 21 ms, Memory: 58.2 MB - LeetHub

--- contains-duplicate-ii/contains-duplicate-ii.java
+++ contains-duplicate-ii/contains-duplicate-ii.java
@@ -3,7 +3,7 @@ class Solution {
         Map<Integer,Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i ++){
             if (map.containsKey(nums[i])){
-                if (Math.abs(map.get(nums[i]) - i) <= k){
+                if (i - map.get(nums[i]) <= k){
                     return true;
                 } 
             }
