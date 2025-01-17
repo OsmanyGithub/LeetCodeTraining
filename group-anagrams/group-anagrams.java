1855ca1238f865e9b8f84e5d1e4582dc52c1bd76
Osmany <158761806+OsmanyGithub@users.noreply.github.com>
2025-01-16 20:54:14 -0800

    Time: 9 ms, Memory: 47.8 MB - LeetHub

--- group-anagrams/group-anagrams.java
+++ group-anagrams/group-anagrams.java
@@ -1,35 +1,22 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // Create a HashMap to store the sorted string as key and the original string as value
         Map<String, List<String>> map = new HashMap<>();
         
-        // Traverse the array 
+        // Iterate the Array strs        
         for (String s: strs){
-            // Create the key
-            char[] stringArray = s.toCharArray();
-            Arrays.sort(stringArray);
-            String key = new String(stringArray);
+            char[] charArray = s.toCharArray(); // Convert to array of characters
+            Arrays.sort(charArray); // Sort the array
+            String key = new String(charArray); // Create the key
             
-            // Create a new list if not present in the map           
+            // Create if the key is already in the map
             if (!map.containsKey(key)){
-                List<String> newList = new ArrayList<>();
-                newList.add(s);
-                map.put(key, newList);
+                map.put(key, new ArrayList<>()); // Create a new list
             }
             
-            // Update the map otherwise
-            else{
-                map.get(key).add(s);
-            }
-            
-            
+            // Update the anagram list
+            map.get(key).add(s);
         }
         
-        List<List<String>> result = new ArrayList<>();
-        for (List<String> list: map.values()){
-            result.add(list);
-        }
-        
-        return result;
+        return new ArrayList<>(map.values());
     }
 }
