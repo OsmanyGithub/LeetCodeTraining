d1dcc9390843c664ee824f49b1a4e40698ce2164
Osmany <158761806+OsmanyGithub@users.noreply.github.com>
2025-01-24 05:06:02 -0800

    Time: 0 ms, Memory: 41.7 MB - LeetHub

--- binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,32 +15,11 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        // One stack approach
         List<Integer> result = new ArrayList<>();
-        if (root == null) return result;
-        
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        
-        while(!stack.isEmpty()){
-            TreeNode node = stack.pop();
-            result.add(node.val);
-            
-            // On this variation we traverse the tree in a modified preorder (root → right → left)            
-            if (node.left != null) stack.push(node.left);            
-            if (node.right != null) stack.push(node.right);
-          
-        }
-        
-        // Reverse result list        
-        int left = 0, right = result.size() -1;
-        while(left < right){
-            int temp = result.get(left);
-            result.set(left, result.get(right));
-            result.set(right, temp);
-            left++;
-            right --;
-            
+        if (root != null){
+            result.addAll(postorderTraversal(root.left));
+            result.addAll(postorderTraversal(root.right));
+            result.add(root.val);
         }
         
         return result;
